894d36021de2a5d88f4b6e6b24e2e9e2bd568920
Chintan <chintan.v.hopiant@gmail.com>
2022-11-11 11:53:24 +0530

    #

--- web/frontend/pages/test.jsx
+++ web/frontend/pages/test.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { slice } from "lodash";
 import { Provider, ResourcePicker } from '@shopify/app-bridge-react';
 import { Button } from '@shopify/polaris';
